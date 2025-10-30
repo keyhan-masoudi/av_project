f04fc54eefd9ce334aa38594eb938f887fb51e32
keyhan masoudi <keyhanmasoudi641383@gmail.com>
2025-10-30 13:57:06 +0330

    add VANET to my project

---
+++ VANET/config.py
@@ -0,0 +1,127 @@
+class Config:
+    CHUNK_SIZE = 2400
+    NEGATIVE_REWARD = -10
+
+    class SimulatorConfig:
+        SIMULATION_DURATION = 1200
+        TIMEOUT_TIME = 2
+        BANDWIDTH = 150
+
+    class CloudConfig:
+        DEFAULT_X = 6000
+        DEFAULT_Y = 1500
+        DEFAULT_RADIUS = 10000
+        CLOUD_BANDWIDTH = 60
+        MAX_TASK_QUEUE_LEN = 2000
+        DEFAULT_COMPUTATION_POWER = 3500
+        POWER_LIMIT = 0.99
+
+    class FixedFogNodeConfig:
+        MAX_TASK_QUEUE_LEN = 400
+        DEFAULT_COMPUTATION_POWER = 500
+        POWER_LIMIT = 0.9
+
+    class MobileFogNodeConfig:
+        DEFAULT_RADIUS = 150
+        MAX_TASK_QUEUE_LEN = 150
+        DEFAULT_COMPUTATION_POWER = 200
+        POWER_LIMIT = 0.6
+
+    class UserNodeConfig:
+        MAX_TASK_QUEUE_LEN = 10
+        DEFAULT_COMPUTATION_POWER = 20
+        # LOCAL_OFFLOAD_POWER_OVERHEAD = 1.1
+        # LOCAL_EXECUTE_TIME_OVERHEAD = 1.1
+        LOCAL_OFFLOAD_POWER_OVERHEAD = 1
+        LOCAL_EXECUTE_TIME_OVERHEAD = 1
+        POWER_LIMIT = 0.4
+
+    class ZoneManagerConfig:
+        ALGORITHM_RANDOM = "Random"
+        ALGORITHM_HEURISTIC = "Heuristic"
+        ALGORITHM_HRL = "HRL"
+        ALGORITHM_ONLY_CLOUD = "Only Cloud"
+        ALGORITHM_ONLY_FOG = "Only Fog"
+        ALGORITHM_DEEP_RL = "DeepRL"
+        ALGORITHM_HEURISTIC2 = "Heuristic2"
+        ALGORITHM_MADDPG = "MADDPG"
+        ALGORITHM_DDPG = "DDPG"
+        ALGORITHM_PPO = "PPO"
+        ALGORITHM_SAC = "SAC"
+
+        DEFAULT_ALGORITHM = ALGORITHM_RANDOM
+
+    class NoiseMethod:
+        PROPOSED_METHOD = "Proposed Method"
+        PROPOSED_METHOD2 = "Proposed Method2"
+        PROPOSED_METHOD3 = "Proposed Method3"
+        FIRST_CHOICE = "First Choice"
+        RANDOM_CHOICE = "Random Choice"
+        MIN_DISTANCE = "Min Distance"
+
+        DEFAULT_METHOD = FIRST_CHOICE
+
+    class RandomZoneManagerConfig:
+        OFFLOAD_CHANCE: float = 0.5
+
+    class AntennaGain:
+        TX: float = 27
+        RX: float = -5
+
+    class TaskConfig:
+        # note: PACKET_COST_PER_METER = 0.001
+        PACKET_COST_PER_METER = 0.001
+        # PACKET_COST_PER_METER = 0.005
+
+        # note: TASK_COST_PER_METER = 0.005
+        # TASK_COST_PER_METER = 0.01
+        TASK_COST_PER_METER = 0.005
+
+        MIGRATION_OVERHEAD = 0.01
+        CLOUD_PROCESSING_OVERHEAD = 0.5
+
+    class NoiseConfig:
+        T1 = 25
+        T2 = 50
+        T3 = 75
+        NONE = -1
+
+        DEFAULT_THRESHOLD = NONE
+
+    class AttenuationLevel:
+        AttenuationLevel1 = [2.3, 2.5, 2.7, 2.9]
+        AttenuationLevel2 = [2.4, 2.7, 3, 3.3]
+        AttenuationLevel1Name = 1
+        AttenuationLevel2Name = 2
+
+        DEFAULT_AttenuationLevel = AttenuationLevel1
+        DEFAULT_AttenuationLevelName = AttenuationLevel1Name
+
+    class TrafficNoise:
+        TrafficNoiseLevel1 = 0
+        TrafficNoiseLevel2 = 1
+        DEFAULT_TrafficNoiseLevel = TrafficNoiseLevel1
+
+        class GreenTrafficNoise:
+            GreenTrafficNoise1 = [95, 91, 90, 89, 85]
+            GreenTrafficNoise2 = [99, 95, 94, 93, 89]
+
+            DEFAULT_GreenTrafficNoise = GreenTrafficNoise1
+
+        class OrangeTrafficNoise:
+            OrangeTrafficNoise1 = [100, 96, 95, 94, 90]
+            OrangeTrafficNoise2 = [102, 98, 97, 96, 92]
+
+            DEFAULT_OrangeTrafficNoise = OrangeTrafficNoise1
+
+        class RedTrafficNoise:
+            RedTrafficNoise1 = [105, 101, 100, 99, 95]
+            RedTrafficNoise2 = [105, 101, 100, 99, 95]
+
+            DEFAULT_RedTrafficNoise = RedTrafficNoise1
+
+        class BlackTrafficNoise:
+            BlackTrafficNoise1 = [110, 106, 105, 104, 100]
+            BlackTrafficNoise2 = [108, 104, 103, 102, 98]
+
+            DEFAULT_BlackTrafficNoise = BlackTrafficNoise1
