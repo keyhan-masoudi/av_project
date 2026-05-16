da4b269fda18da38de366e5d76e19b3edd0bdde6
AbbasTestAccount <abbasfake5@gmail.com>
2026-05-16 13:11:40 +0330

    fix random algorithm

--- controllers/zone_managers/random.py
+++ controllers/zone_managers/random.py
@@ -1,6 +1,5 @@
 import random
-
-from typing import Dict, List, Unpack
+from typing import Dict, List, Unpack, Any
 
 from config import Config
 from controllers.zone_managers.base import ZoneManagerABC, ZoneManagerUpdate
@@ -9,24 +8,37 @@ from models.task import Task
 
 
 class RandomZoneManager(ZoneManagerABC):
+    def __init__(self, zone):
+        super().__init__(zone)
+        self.simulator = None
+
+    def set_simulator(self, simulator):
+        self.simulator = simulator
+
     def can_offload_task(self, task: Task) -> bool:
         merged_fog_nodes: Dict[str, FogLayerABC] = {**self.fixed_fog_nodes, **self.mobile_fog_nodes}
 
-        possible_fog_nodes: List[FogLayerABC] = []
+        possible_nodes = []
+
         if task.creator.can_offload_task(task):
-            possible_fog_nodes.append(task.creator)
+            possible_nodes.append(task.creator)
+
         for fog_id, fog in merged_fog_nodes.items():
             if fog.can_offload_task(task):
-                possible_fog_nodes.append(fog)
+                possible_nodes.append(fog)
 
-        if len(possible_fog_nodes) == 0:
+        if self.simulator and self.simulator.cloud_node:
+            if self.simulator.cloud_node.can_offload_task(task):
+                possible_nodes.append(self.simulator.cloud_node)
+
+        if len(possible_nodes) == 0:
             return False
-        self.__possible_fog_nodes = possible_fog_nodes
-        # return random.random() < 1 # to check count of zone_manager_offload_task and zoneManagers are equal or not
+
+        self.__possible_nodes = possible_nodes
         return random.random() < Config.RandomZoneManagerConfig.OFFLOAD_CHANCE
 
     def update(self, **kwargs: Unpack[ZoneManagerUpdate]):
         pass
 
-    def assign_task(self, task: Task) -> FogLayerABC:
-        return random.choice(self.__possible_fog_nodes)
+    def assign_task(self, task: Task) -> Any:
+        return random.choice(self.__possible_nodes)
